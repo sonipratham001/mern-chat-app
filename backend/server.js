11787f384f951d0ebc99293ec830bd29a027ec40
sonipratham001 <sonipratham3008@gmail.com>
2024-06-14 18:10:49 +0530

    Deployment

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from 'cookie-parser';
-
+import path from "path";
 
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
@@ -12,6 +12,8 @@ import { app, server } from "./soket/soket.js";
 
 const PORT = process.env.PORT || 5001;
 
+const __dirname = path.resolve();
+
 dotenv.config();
 
 app.use(express.json()); // to parse the incoming requests with JSON payloads (from req.body)
@@ -20,11 +22,13 @@ app.use("/api/auth", authRoutes)
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
+app.use(express.static(path.join(__dirname,"/frontend/dist")));
 
-// app.get("/", (req,res)=> {
-//     // route route http://localhost:5001/
-//     res.send("Hello World!!");
-// });
+app.get("*", (req,res) => {
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+})
+
+
 
 
 server.listen(PORT, () => {
